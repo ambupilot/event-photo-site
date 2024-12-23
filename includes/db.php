558f54a88962711e2d95f44c27e2e1c059fce1b6
Martijn Kerssing <martijn@kerssing.nl>
2024-12-23 14:58:51 +0100

    db switch

--- includes/db.php
+++ includes/db.php
@@ -1,4 +1,38 @@
 <?php
+// Controleer het gebruikte domein
+$host = $_SERVER['HTTP_HOST'];
+
+// Database-instellingen
+if ($host === 'event-photo-site') {
+    // Instellingen voor de testserver
+    $host = 'localhost';
+    $dbname = 'kerssing_snapshowprint';
+    $username = 'martijn'; // Pas dit aan naar je MySQL-gebruikersnaam
+    $password = 'kerssing'; // Pas dit aan naar je MySQL-wachtwoord
+    echo "Verbinding maken met de testserver...\n";
+} else {
+    // Instellingen voor de liveserver
+    $host = 'localhost';
+    $dbname = 'njdvuexwzd';
+    $username = 'njdvuexwzd'; // Pas dit aan naar je MySQL-gebruikersnaam
+    $password = 'pZ479uhzV4'; // Pas dit aan naar je MySQL-wachtwoord
+    echo "Verbinding maken met de liveserver...\n";
+}
+
+// Maak een databaseverbinding
+try {
+    $dsn = "mysql:host=$dbHost;dbname=$dbName;charset=utf8mb4";
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Verbinding succesvol!";
+} catch (PDOException $e) {
+    echo "Fout bij het verbinden met de database: " . $e->getMessage();
+}
+
+
+/*
+// oude local/test verbinfing
+
 $host = 'localhost';
 $dbname = 'kerssing_snapshowprint';
 $username = 'martijn'; // Pas dit aan naar je MySQL-gebruikersnaam
@@ -10,4 +44,5 @@ try {
 } catch (PDOException $e) {
     die("Databaseverbinding mislukt: " . $e->getMessage());
 }
+    */
 ?>
